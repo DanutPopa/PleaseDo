c75f4458a1e88e5c3d6f68ceebbd8921549202fc
Danut Popa <popa.ion.danut@gmail.com>
2025-10-17 19:10:52 +0300

    Add project to repo.

---
+++ PleaseDo/PleaseDo/PleaseDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoApp.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
