e7adabe3df674d835613397cec2619e22d928941
Danut Popa <popa.ion.danut@gmail.com>
2025-10-18 15:41:04 +0300

    Add ListView to project.

---
+++ PleaseDo/PleaseDo/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 18.10.2025.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ListView()
+}
