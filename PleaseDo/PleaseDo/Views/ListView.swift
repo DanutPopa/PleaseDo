516c1418013eb69ddfc581497190e04158a54764
Danut Popa <popa.ion.danut@gmail.com>
2025-10-18 16:22:06 +0300

    Add basic ListView layout.

--- PleaseDo/PleaseDo/Views/ListView.swift
+++ PleaseDo/PleaseDo/Views/ListView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ListView: View {
+    let title: String
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 0) {
+            Text(title)
+                .font(.title2)
+                .fontWeight(.medium)
+                .frame(maxWidth: .infinity, maxHeight: 32)
+                .background {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundStyle(.orange)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.white, lineWidth: 2)
+                        }
+                }
+                .padding([.horizontal, .top])
+            
+            List {
+                ForEach(1..<4) { index in
+                    Text("Item \(index)")
+                }
+            }
+            .scrollContentBackground(.hidden)
+            .listStyle(.insetGrouped)
+        }
     }
 }
 
 #Preview {
-    ListView()
+    ListView(title: "To Do")
 }
