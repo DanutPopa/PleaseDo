c3020900c7004ede5584e6efd64df0139e649c56
Danut Popa <popa.ion.danut@gmail.com>
2025-10-18 16:52:25 +0300

    Add Item data model to project.

---
+++ PleaseDo/PleaseDo/Models/Item.swift
@@ -0,0 +1,48 @@
+//
+//  Item.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 18.10.2025.
+//
+
+import Foundation
+
+struct Item: Identifiable {
+    let id: String
+    let authorId: String
+    var title: String
+    var description: String
+    var startDate: Date = .now
+}
+
+enum Status: String {
+    case todo = "To Do"
+    case inProgress = "In Progress"
+    case done = "Done"
+    case unknown = "Unknown"
+    
+    init(_ str: String) {
+        self = switch str.lowercased() {
+        case "todo": .todo
+        case "in progress": .inProgress
+        case "done": .done
+        default: .unknown
+        }
+    }
+}
+
+enum Priority: String {
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+    case unknown = "Unknown"
+    
+    init(_ str: String) {
+        self = switch str.lowercased() {
+        case "low": .low
+        case "medium": .medium
+        case "high": .high
+        default: .unknown
+        }
+    }
+}
