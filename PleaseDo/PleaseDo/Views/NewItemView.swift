b4ebb81ab8b0bb2af3fdd3ac00d18ab949d870aa
Danut Popa <popa.ion.danut@gmail.com>
2025-10-19 19:23:18 +0300

    Add NewItemView to project.

---
+++ PleaseDo/PleaseDo/Views/NewItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NewItemView.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 19.10.2025.
+//
+
+import SwiftUI
+
+struct NewItemView: View {
+    var body: some View {
+        Text("Create new item here")
+    }
+}
+
+#Preview {
+    NewItemView()
+}
