d7f8335ae905dc02810a1526dc0100555cbbe4c9
Danut Popa <popa.ion.danut@gmail.com>
2025-10-20 16:24:21 +0300

    Create TitleTextField and use it in NewItemView.

---
+++ PleaseDo/PleaseDo/Views/TitledTextField.swift
@@ -0,0 +1,28 @@
+//
+//  TitledTextField.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 20.10.2025.
+//
+
+import SwiftUI
+
+struct TitledTextField: View {
+    let title: String
+    let placeholder: String
+    @Binding var text: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.caption2)
+                .foregroundColor(.secondary)
+            
+            TextField(placeholder, text: $text)
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    TitledTextField(title: "Title", placeholder: "What do you need to do?", text: .constant(""))
+}
