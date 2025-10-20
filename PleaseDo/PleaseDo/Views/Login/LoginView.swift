1d0ae7b419601ef50ec60ffe16d6b6eab4f1e930
Danut Popa <popa.ion.danut@gmail.com>
2025-10-20 19:37:36 +0300

    Add LoginTitleView.

---
+++ PleaseDo/PleaseDo/Views/Login/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 20.10.2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Spacer()
+            
+            LoginTitleView()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
