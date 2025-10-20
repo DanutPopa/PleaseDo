f80c112652132cf19e0e2cd596892c96b51e6b4f
Danut Popa <popa.ion.danut@gmail.com>
2025-10-20 18:34:17 +0300

    Adjust ItemDetailsView.

--- PleaseDo/PleaseDo/Views/ItemDetailsView.swift
+++ PleaseDo/PleaseDo/Views/ItemDetailsView.swift
@@ -9,20 +9,37 @@ import SwiftUI
 
 struct ItemDetailsView: View {
     let item: Item
+    @State private var updatedItem = Item.example
     
     var body: some View {
         VStack {
-            Text(item.title)
+            TitledTextField(title: "Title", placeholder: "What do you need to do ?", text: $updatedItem.title)
             
-            Text(item.description)
+            Divider()
             
-            Text(item.status.rawValue)
+            TitledTextField(title: "Description", placeholder: "Add a brief description", text: $updatedItem.description)
             
-            Text(item.priority.rawValue)
+            Divider()
+            
+            StatusMenu(status: $updatedItem.status)
+            
+            Divider()
+            
+            PriorityMenu(priority: $updatedItem.priority)
+            
+            Spacer()
+            
+            CTAButton(title: "Confirm") {
+                print("CTAButton tapped")
+            }
         }
+        .padding()
+        .navigationTitle("Details")
     }
 }
 
 #Preview {
-    ItemDetailsView(item: .example)
+    NavigationStack {
+        ItemDetailsView(item: .example)
+    }
 }
