8b30049f686b34889c78f35493ec5258fce1d12f
Danut Popa <popa.ion.danut@gmail.com>
2025-10-24 19:13:29 +0300

    Setup Auth State Listener.

--- PleaseDo/PleaseDo/Views/Login/LoginManager.swift
+++ PleaseDo/PleaseDo/Views/Login/LoginManager.swift
@@ -13,7 +13,16 @@ final class LoginManager {
     // Fetch login related data to pass to LoginVM
     
     private let db = Firestore.firestore()
+    private var handler: AuthStateDidChangeListenerHandle?
     var currentUser: User?
+    
+    init() {
+        setupListener()
+    }
+    
+    deinit {
+        removeListener()
+    }
     
     func signUP(_ firstName: String, lastName: String, _ email: String, _ password: String) {
         Task {
@@ -32,5 +41,29 @@ final class LoginManager {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            currentUser = nil
+            removeListener()
+            print("Successfully signed out user!")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupListener() {
+        guard handler == nil else { return }
+        handler = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            guard let self else { return }
+            currentUser = auth.currentUser
+        }
+    }
+    
+    func removeListener() {
+        guard let handler else { return }
+        Auth.auth().removeStateDidChangeListener(handler)
     }
 }
