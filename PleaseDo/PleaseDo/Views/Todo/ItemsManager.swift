e194e42b208c97b7ec77ce2891fcd580b6eb03f0
Danut Popa <popa.ion.danut@gmail.com>
2025-10-25 17:17:20 +0300

    Add ItemsManager.

---
+++ PleaseDo/PleaseDo/Views/Todo/ItemsManager.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsManager.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 25.10.2025.
+//
+
+import Foundation
+
+final class ItemsManager {
+    static let shared = ItemsManager()
+    
+    private init() {}
+}
