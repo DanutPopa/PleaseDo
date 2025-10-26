6cda6140d9c3a7f5dcfc8738d746c499e4e21597
Danut Popa <popa.ion.danut@gmail.com>
2025-10-26 19:00:22 +0200

    Finish Item Fetching Logic.

--- PleaseDo/PleaseDo/ViewModels/ListVM.swift
+++ PleaseDo/PleaseDo/ViewModels/ListVM.swift
@@ -43,14 +43,72 @@ extension ListVM: ItemsManagerDelegate {
     }
     
     func didAddItem(_ item: Item) {
-        
+        moveItem(item)
+    }
+    
+    func moveItem(_ item: Item) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            switch item.status {
+            case .todo:
+                todoItems.append(item)
+            case .inProgress:
+                inProgressItems.append(item)
+            case .done:
+                doneItems.append(item)
+            case .unknown:
+                break
+            }
+        }
     }
     
     func didUpdateItem(_ item: Item) {
-        
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            if let index = todoItems.firstIndex(of: item) {
+                if todoItems[index].status == item.status {
+                    todoItems[index] = item
+                } else {
+                    todoItems.remove(at: index)
+                    moveItem(item)
+                }
+            } else if let index = inProgressItems.firstIndex(of: item) {
+                if inProgressItems[index].status == item.status {
+                    inProgressItems[index] = item
+                } else {
+                    inProgressItems.remove(at: index)
+                    moveItem(item)
+                }
+            } else if let index = doneItems.firstIndex(of: item) {
+                if doneItems[index].status == item.status {
+                    doneItems[index] = item
+                } else {
+                    doneItems.remove(at: index)
+                    moveItem(item)
+                }
+            }
+        }
     }
     
     func didDeleteItem(_ item: Item) {
-        
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            switch item.status {
+            case .todo:
+                if let index = todoItems.firstIndex(of: item) {
+                    todoItems.remove(at: index)
+                }
+            case .inProgress:
+                if let index = inProgressItems.firstIndex(of: item) {
+                    inProgressItems.remove(at: index)
+                }
+            case .done:
+                if let index = doneItems.firstIndex(of: item) {
+                    doneItems.remove(at: index)
+                }
+            case .unknown:
+                break
+            }
+        }
     }
 }
