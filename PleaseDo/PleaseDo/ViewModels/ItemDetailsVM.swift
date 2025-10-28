5d75a382f0f81ec8dacf7d75b9149e4d2df3945d
Danut Popa <popa.ion.danut@gmail.com>
2025-10-28 13:43:00 +0200

    Add a view model for ItemDetailsView.

---
+++ PleaseDo/PleaseDo/ViewModels/ItemDetailsVM.swift
@@ -0,0 +1,13 @@
+//
+//  ItemDetailsVM.swift
+//  PleaseDo
+//
+//  Created by Danut Popa on 28.10.2025.
+//
+
+import Foundation
+import Combine
+
+final class ItemDetailsVM: ObservableObject {
+    
+}
